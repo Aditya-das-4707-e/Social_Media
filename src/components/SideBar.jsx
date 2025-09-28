a26c27e022090ee718214b4994de8bd4bbe7a622
Aditya Das <adityadas99906@gmail.com>
2025-09-28 08:29:56 +0530

    UI update

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SideBar = () => {
   return (
-    <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style={{ width: '280px', height: '100vh' }}>
+    <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar" style={{ width: '280px', height: '100vh' }}>
       <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
         <svg className="bi pe-none me-2" width="40" height="32" aria-hidden="true">
           <use xlinkHref="#bootstrap"></use>
@@ -24,31 +24,7 @@ const SideBar = () => {
             <svg className="bi pe-none me-2" width="16" height="16" aria-hidden="true">
               <use xlinkHref="#speedometer2"></use>
             </svg>
-            Dashboard
-          </a>
-        </li>
-        <li>
-          <a href="#" className="nav-link text-white">
-            <svg className="bi pe-none me-2" width="16" height="16" aria-hidden="true">
-              <use xlinkHref="#table"></use>
-            </svg>
-            Orders
-          </a>
-        </li>
-        <li>
-          <a href="#" className="nav-link text-white">
-            <svg className="bi pe-none me-2" width="16" height="16" aria-hidden="true">
-              <use xlinkHref="#grid"></use>
-            </svg>
-            Products
-          </a>
-        </li>
-        <li>
-          <a href="#" className="nav-link text-white">
-            <svg className="bi pe-none me-2" width="16" height="16" aria-hidden="true">
-              <use xlinkHref="#people-circle"></use>
-            </svg>
-            Customers
+            Create Post
           </a>
         </li>
         
